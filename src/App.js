803ac6f57c2e84a33fe16bc4a815ddfb8093b862
James Trbovich <jim.trbovich@gmail.com>
2018-02-13 00:06:08 -0600

    imported todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getTodo } from './actions/todoActions';
+import { getTodos } from './actions/todoActions';
 // import './components/redux'
 
 class App extends Component {
   componentWillMount() {
-    this.props.dispatch({type: 'THIS_BETTER_WORK', payload: 'Go to the store'})
-    this.props.dispatch(getTodo())
+    // this.props.dispatch({type: 'THIS_BETTER_WORK', payload: 'Go to the store'})
+    // this.props.dispatch(getTodo())
+    this.props.dispatch(getTodos())
   }
 
+
   render() {
+    if(Object.getOwnPropertyNames(this.props.todos).length > 0){
+      console.log(lastTodo);
+    }
+    var lastTodo = this.props.todos.pop()
     return (
       <div className="App">
         <h2>Redux Training Is Fun</h2>
-        <span>{this.props.todo}</span>
+        <span></span>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  todo: state.todo
+  todos: state.todos
 });
 
 export default connect(mapStateToProps)(App)
--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -5,6 +5,15 @@ import axios from 'axios';
 //   payload: axios.get("https://jsonplaceholder.typicode.com/todos")
 // })
 
+export function getTodos() {
+  return function(dispatch){
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        dispatch({type: 'FETCH_USERS_FULFILLED', payload: response.data});
+      })
+  }
+}
+
 export function getTodo() {
   return {type: 'FETCH_USERS', payload: 'Pay bills'}
 }
